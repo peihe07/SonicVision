49b34b7f39121eca3ac62609569d8993cafc2019
peiyuhsu <y450376@gmail.com>
2025-03-01 17:48:00 +0800

    250301_18

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def vue_index(request):
     return render(request, 'index.html')  # ✅ 確保載入 Vue 的 index.html
+
+def search_tmdb(request):
+    return JsonResponse({"message": "API 正常運作", "results": []})
