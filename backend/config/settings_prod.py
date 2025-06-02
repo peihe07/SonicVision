ac8f473c6963ba02ee85768fbea7b5b4aab32d7c
peiyuhsu <y450376@gmail.com>
2025-06-02 21:51:18 +0800

    fix: 更新 CORS 設定，添加更多允許的標頭

--- backend/config/settings_prod.py
+++ backend/config/settings_prod.py
@@ -37,6 +37,10 @@ CORS_ALLOW_HEADERS = [
     'user-agent',
     'x-csrftoken',
     'x-requested-with',
+    'access-control-allow-origin',
+    'access-control-allow-headers',
+    'access-control-allow-methods',
+    'access-control-allow-credentials',
 ]
 
 # 允許憑證
